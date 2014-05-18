4a481fd905142a931f6dd8f1a831b0570e209dae
dominioYP <dominioYP@gmail.com>
2014-05-18 17:21:23 +0200

    .

--- Clever_Buy/src/it/polito/ai/project/andoidside/fragment/InserisciUnProdottoFragment.java
+++ Clever_Buy/src/it/polito/ai/project/andoidside/fragment/InserisciUnProdottoFragment.java
@@ -1,5 +1,7 @@
 package it.polito.ai.project.andoidside.fragment;
 
+import java.util.Calendar;
+
 import it.polito.ai.project.andoidside.R;
 import android.app.Fragment;
 import android.content.Intent;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +22,7 @@ public class InserisciUnProdottoFragment extends Fragment {
 	private Button _scan;
 	private EditText _etBarcode;
 	
+	private DatePicker _data_inizio, _data_fine;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -27,6 +31,9 @@ public class InserisciUnProdottoFragment extends Fragment {
 
 		_scan = (Button) rootView.findViewById(R.id.iup_bc_btnScan);
 		_etBarcode  = (EditText) rootView.findViewById(R.id.iup_editText2);
+		
+		_data_inizio  = (DatePicker) rootView.findViewById(R.id.iup_dp_data_inizio);
+		_data_fine    = (DatePicker) rootView.findViewById(R.id.iup_dp_data_fine);
 		
 		addListner();
 		
@@ -59,6 +66,23 @@ public class InserisciUnProdottoFragment extends Fragment {
 
 			}
 		});
+		
+		OnClickListener data_inizio_fine = new OnClickListener() { 			
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stubcal
+					Calendar cal = Calendar.getInstance();
+					cal.set(Calendar.YEAR, _data_inizio.getYear());
+					cal.set(Calendar.MONTH, _data_inizio.getMonth());
+					cal.set(Calendar.DAY_OF_MONTH, _data_inizio.getDayOfMonth());
+					_data_fine.setMinDate( cal.getTimeInMillis());
+				}
+			}; 
+		
+			_data_inizio.setOnClickListener( data_inizio_fine);
+			_data_fine.setOnClickListener( data_inizio_fine);
+			
+		
 	}
 	
 	//In the same activity you’ll need the following to retrieve the results:
